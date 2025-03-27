0153c9173f4c98cff56d39ea4ab067298ea17e11
Aditya <21102034@ittelkom-pwt.ac.id>
2025-03-28 04:31:16 +0700

    Base

---
+++ BE/src/models/User.js
@@ -0,0 +1,28 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/db.js";
+
+export const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  googleId: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: true, // Bisa null untuk user yang daftar via Google
+  },
+});
